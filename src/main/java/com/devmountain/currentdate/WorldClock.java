58b2b8cc18a9c9b2c73de0e1c01e11012bfc2f38
Robert Davis <robdave22@yahoo.com>
2021-12-30 10:22:12 -0500

    .gitignore added

---
+++ src/main/java/com/devmountain/currentdate/WorldClock.java
@@ -0,0 +1,49 @@
+package com.devmountain.currentdate;
+
+import java.time.*;
+
+public class WorldClock {
+
+    public LocalDate getNowForDate() {
+        return null;
+    }
+
+    public LocalDateTime getNowForDateAndTime() {
+        return null;
+    }
+
+    public DayOfWeek getDayOfWeekForNow() {
+        return null;
+    }
+
+    public int getDayOfMonthForNow() {
+        return 0;
+    }
+
+    public int getDayOfYearForNow() {
+        return 0;
+    }
+
+    public ZonedDateTime getNowDateTimeForNewYork() {
+        return null;
+    }
+
+    public ZonedDateTime getNowDateTimeForLA() {
+        return null;
+    }
+
+    public ZonedDateTime getNowDateTimeForLondon() {
+        return null;
+    }
+
+    public ZonedDateTime getNowDateTimeForMoscow() {
+        return null;
+    }
+
+    public ZonedDateTime getNowDateTimeForTokyo() {
+        return null;
+    }
+
+
+
+}
